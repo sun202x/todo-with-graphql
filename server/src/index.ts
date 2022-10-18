dae5f73a0a2ff8c0834eec5a1cb5335785452074
sun202x <sun202x@gmail.com>
2022-10-18 17:13:57 +0900

    init: mongoDB 설정

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,48 @@
 import { ApolloServer } from 'apollo-server';
 import resolvers from './resolver';
+import { config } from 'dotenv';
+import { MongoClient, Db } from 'mongodb';
 
-const { readFileSync } = require('fs');
-const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8');
+async function startAsync() {
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+    // 환경설정 사용
+    config();
+    
+    const MONGO_DB = process.env.DB_HOST;
+    let db: Db;
+    
+    try {
+        if (MONGO_DB) {
+            const client = await MongoClient.connect(MONGO_DB, {
+                
+            });
+            db = client.db();
+        }
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+    } catch (error) {
+        console.log(`
+            Mongo DB Host not found!
+            please add DB_HOST environment variable to .env file
+    
+            exiting...
+        `);
+        process.exit(1);
+    }
+    
+    const { readFileSync } = require('fs');
+    const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8');
+    
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: () => {
+        return { db };
+      }
+    });
+    
+    server.listen().then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
+}
+
+startAsync();
