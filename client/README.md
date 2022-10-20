3e2fbe188c83e31c7384b40cda51c8671411e2bd
sun202x <sun202x@gmail.com>
2022-10-20 09:07:12 +0900

    init: client 환경구성

---
+++ client/README.md
@@ -0,0 +1,2 @@
+# todo-with-graphql(client)
+graphQL 응용 클라이언트 프로젝트
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      일단은 App입니다.
+    </div>
+  );
+}
+
+export default App;
